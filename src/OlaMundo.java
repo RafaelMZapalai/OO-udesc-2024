a64f8dad4e00fc5429e89f100c344d36ecbf07d3
Rafael Zapalai <10880893990@edu.udesc.br>
2024-08-19 21:49:03 -0300

    classe OlaMundo e teste

---
+++ src/OlaMundo.java
@@ -0,0 +1,10 @@
+public class OlaMundo {
+    String nome = "udesc!";
+    int idade = 42;
+    void imprimirdados(){
+        System.out.println(nome + "-" + idade);
+    }
+    void imprimir(String nome,int idade){
+        System.out.println(nome + "-" + idade);
+    }
+}
---
+++ src/TestaOlaMundo.java
@@ -0,0 +1,8 @@
+public class TestaOlaMundo {
+    public static void main(String[] args) {
+        // cria objeto apartir da classe
+        OlaMundo ola = new OlaMundo();
+        ola.imprimirdados();
+        ola.imprimir("Fulano", 18);
+    }
+}
